8f0c2bcf48a80ef8f9a79fe2e736d14fc28141dd
Kumar1872 <saikumar.madala@symphonytalent.com>
2023-04-16 15:24:11 +0530

    auth test

--- app.js
+++ app.js
@@ -89,7 +89,7 @@ passport.deserializeUser(function(id, done) {
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://lazy-blue-viper-shoe.cyclic.app/secrets",
+    callbackURL: "https://lazy-blue-viper-shoe.cyclic.app/secrets",
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
   },
   function(accessToken, refreshToken, profile, done) {
